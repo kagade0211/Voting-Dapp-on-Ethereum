3534267442049cd9ae1b7f9a7c44dba66f3832ca
kagade0211 <kagadeakshay0211@gmail.com>
2022-02-20 16:57:56 +0530

    successfully migrated contract with addCandidate function

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.4.11;
+pragma solidity ^0.5.16;
 
 contract Election {
   //model candidate
@@ -16,15 +16,15 @@ contract Election {
 // constructor defination in old version of solidity. In newer version you donot need to add function keyword and functionName
 //Constructor gets called automatically once contract is migrated on blockchain.
 //smart contract is in control of candidates addition. externally we cannot add candidate
-  function Election () public {
+  constructor() public {
     addCandidate("Candidate 1");
     addCandidate("Candidate 2");
   }
-  function addCandidate (string _name) private {
+  function addCandidate (string memory _name) private {
     //function is private so that noone will be able to add new candidate after deployment
     //candidesCount is increased first and is passed as uint which is id
     candidatesCount ++;
     //candidate is array and we are passing  candidateCount as array no.
-    candidates[candidatesCount] = Candidate(candidantesCount, _name,0);
+    candidates[candidatesCount] = Candidate(candidatesCount, _name,0);
   }
 }
