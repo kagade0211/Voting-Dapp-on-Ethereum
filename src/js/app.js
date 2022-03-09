734ee08d513d352cbb93f7a1d5464552b417c21c
kagade0211 <kagadeakshay0211@gmail.com>
2022-03-09 20:22:21 +0530

    add a form that allows accounts to vote below the table in our index.html file

--- src/js/app.js
+++ src/js/app.js
@@ -40,49 +40,73 @@ App = {
     });
   },
 //render function layout all content on page. 1. display accounts connected to blockchain 2. list out candidates in election
-  render: function() {
-    var electionInstance;
-    var loader = $("#loader");
-    var content = $("#content");
+render: function() {
+  var electionInstance;
+  var loader = $("#loader");
+  var content = $("#content");
 
-    loader.show();
-    content.hide();
+  loader.show();
+  content.hide();
 
-    // Load account data 
-    web3.eth.getCoinbase(function(err, account) {
-      if (err === null) {
-        App.account = account;
-        $("#accountAddress").html("Your Account: " + account);
-      }
-    });
+  // Load account data
+  web3.eth.getCoinbase(function(err, account) {
+    if (err === null) {
+      App.account = account;
+      $("#accountAddress").html("Your Account: " + account);
+    }
+  });
 
-    // Load contract data
-    App.contracts.Election.deployed().then(function(instance) {
-      electionInstance = instance;
-      return electionInstance.candidatesCount();
-    }).then(function(candidatesCount) {
-      var candidatesResults = $("#candidatesResults");
-      candidatesResults.empty();
+  // Load contract data
+  App.contracts.Election.deployed().then(function(instance) {
+    electionInstance = instance;
+    return electionInstance.candidatesCount();
+  }).then(function(candidatesCount) {
+    var candidatesResults = $("#candidatesResults");
+    candidatesResults.empty();
 
-      for (var i = 1; i <= candidatesCount; i++) {
-        electionInstance.candidates(i).then(function(candidate) {
-          var id = candidate[0];
-          var name = candidate[1];
-          var voteCount = candidate[2];
+    var candidatesSelect = $('#candidatesSelect');
+    candidatesSelect.empty();
 
-          // Render candidate Result
-          var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
-          candidatesResults.append(candidateTemplate);
-        });
-      }
+    for (var i = 1; i <= candidatesCount; i++) {
+      electionInstance.candidates(i).then(function(candidate) {
+        var id = candidate[0];
+        var name = candidate[1];
+        var voteCount = candidate[2];
 
-      loader.hide();
-      content.show();
-    }).catch(function(error) {
-      console.warn(error);
-    });
-  }
-};
+        // Render candidate Result
+        var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
+        candidatesResults.append(candidateTemplate);
+
+        // Render candidate ballot option
+        var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
+        candidatesSelect.append(candidateOption);
+      });
+    }
+    return electionInstance.voters(App.account);
+  }).then(function(hasVoted) {
+    // Do not allow a user to vote
+    if(hasVoted) {
+      $('form').hide();
+    }
+    loader.hide();
+    content.show();
+  }).catch(function(error) {
+    console.warn(error);
+  });
+}
+
+castVote: function() {
+  var candidateId = $('#candidatesSelect').val();
+  App.contracts.Election.deployed().then(function(instance) {
+    return instance.vote(candidateId, { from: App.account });
+  }).then(function(result) {
+    // Wait for votes to update
+    $("#content").hide();
+    $("#loader").show();
+  }).catch(function(err) {
+    console.error(err);
+  });
+}
 
 $(function() {
   //initialise app as soon as window loads
